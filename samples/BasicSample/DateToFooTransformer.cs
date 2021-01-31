07f5fcc4907be1a08a8ceedcf50a15e8cdf3c437
Tyler Davis <tgfdavis98@gmail.com>
2021-01-31 15:33:22 +0000

    fix samples

--- samples/BasicSample/DateToFooTransformer.cs
+++ samples/BasicSample/DateToFooTransformer.cs
@@ -3,13 +3,19 @@ using Reform;
 
 namespace BasicSample
 {
-    // public class DateToFooTransformer : ITransformer<Foo, DateTime>
-    // {
-    //     public Foo Transform(DateTime from)
-    //     {
-    //         return new Foo {Date = from};
-    //     }
-    // }
+    public class DateToFooTransformer : ITransformer<Foo, DateTime>
+    {
+        public Foo Transform(DateTime from)
+        {
+            return new Foo {Date = from};
+        }
+
+        public bool TryTransform(DateTime from, out Foo to)
+        {
+            to = Transform(from);
+            return true;
+        }
+    }
 
     public class Foo
     {
--- samples/BasicSample/Program.cs
+++ samples/BasicSample/Program.cs
@@ -7,8 +7,18 @@ namespace BasicSample
     {
         static void Main(string[] args)
         {
-            var b = DateTime.Now.TryTransform<DateTime, Foo>(out var c);
-            Console.WriteLine(b);
+            var fooValue = DateTime.Now.Transform<DateTime, Foo>();
+            Console.WriteLine(fooValue.Date);
+            // or in case it could fail, you can try:
+
+            if (DateTime.Now.TryTransform<DateTime, Foo>(out var fooObj))
+            {
+                Console.WriteLine(fooObj.Date);
+            }
+            else
+            {
+                Console.WriteLine("):");
+            }
         }
     }
 }
